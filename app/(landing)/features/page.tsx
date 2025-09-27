09706211da96d5a5f3e7f87806246596e824936c
BIXSCON <viseytech@gmail.com>
2025-09-27 19:28:21 +0530

    Update page.tsx
    Updated Syntax errors

--- app/(landing)/features/page.tsx
+++ app/(landing)/features/page.tsx
@@ -130,14 +130,40 @@ const page = () => {
                   with a qualified, engaged audience.
                 </p>
               </div>
+              <div>
+                <h2 className="font-semibold">Marketing Analytics Dashboard</h2>
+                <p className="font-medium">
+                  Leverage data-driven insights to better understand your
+                  audience. Track engagement, identify what resonates, and
+                  optimize your marketing strategies to meet the needs of Indian
+                  entrepreneurs.
+                </p>
+              </div>
+              <div>
+                <h2 className="font-semibold">Cut Promotional Costs</h2>
+                <p className="font-medium">
+                  Promote your products and resources directly to a qualified
+                  audience of startups and MSMEs. Save significantly on
+                  traditional marketing costs while driving meaningful
+                  engagement.
+                </p>
               </div>
             </div>
-        </div>
-
+            <div className="w-[320px] md:w-[630px] h-[350px] relative">
+              <Image
+                src="/img/Resource-4.webp"
+                alt="Resource-4.webp"
+                fill={true}
+                objectFit="contain"
+                priority
+              />
+            </div>
+          </div>
+         
       <div className="flex items-center justify-center pt-16 pb-16">
         <Button>
           <div className="flex items-center gap-2">
-            <Link href="/login" className="flex items-center gap-4">
+            <Link href="/demo-account-type" className="flex items-center gap-4">
               <span>Start now</span>
               <ArrowUpRight />
             </Link>
