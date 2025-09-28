df405fc58ca4d6211c6c53c5faf07a9d9a4c93e3
BIXSCON <viseytech@gmail.com>
2025-09-28 17:44:37 +0530

    Update EmblaCarousel.tsx
    Fixed version

--- components/EmblaCarousel.tsx
+++ components/EmblaCarousel.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 import { EmblaOptionsType } from 'embla-carousel'
 import useEmblaCarousel from 'embla-carousel-react'
 import AutoScroll from 'embla-carousel-auto-scroll'
-import TestimonialCard from '@/components/cards/TestimonialCard';
+import TestimonialCard from '@/components/cards/TestimonialCard'
 import { DotButton, useDotButton } from './EmblaCarouselDotButton'
 
 interface Testimonial {
-  image: string;
-  content: string;
+  image: string
+  content: string
 }
 
 type PropType = {
@@ -21,14 +21,13 @@ type PropType = {
 const EmblaCarousel: React.FC<PropType> = (props) => {
   const { slides, options, className } = props
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [
-    AutoScroll({ playOnInit: true })
+    AutoScroll({ playOnInit: true }),
   ])
 
-  const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi)
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi)
 
   return (
-    <div className={max-w-[320px] ${className || ''}}>
+    <div className={`max-w-[320px] ${className || ''}`}>
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="flex flex-row p-3">
           {slides.map((slide, index) => (
@@ -44,7 +43,11 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
               <DotButton
                 key={index}
                 onClick={() => onDotButtonClick(index)}
-                className={w-3 h-3 rounded-full ${index === selectedIndex ? 'bg-primary-landing' : 'bg-[#D9D9D9]'}}
+                className={`w-3 h-3 rounded-full ${
+                  index === selectedIndex
+                    ? 'bg-primary-landing'
+                    : 'bg-[#D9D9D9]'
+                }`}
               />
             ))}
           </div>
